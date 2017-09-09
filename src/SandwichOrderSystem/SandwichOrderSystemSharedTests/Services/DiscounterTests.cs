b026c297ec5ebc7b7aea4b1be17bb4f9e2fc1922
David Lin <david.lin@bmwna.com>
2017-09-08 21:07:55 -0600

    add unit tests DiscounterTests, stub out OrderManagerTests

---
+++ src/SandwichOrderSystem/SandwichOrderSystemSharedTests/Services/DiscounterTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SandwichOrderSystemShared.DataAccess.Deserializer;
+using SandwichOrderSystemShared.Models;
+using SandwichOrderSystemShared.Models.Items;
+
+namespace SandwichOrderSystemShared.Services.Tests
+{
+    [TestClass()]
+    public class DiscounterTests
+    {
+        IItemFactory itemFactory = new ItemFactory();
+        IDiscounter discounter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            discounter = new Discounter();
+        }
+
+        [TestMethod()]
+        public void GetDiscountItemConditionallyTest_HasDrinkAndChipsReturnsDiscountItem()
+        {
+            var order = new Order();
+            var drink = itemFactory.CreateItem<Drink>(new string[] { "drink", "1.0" });
+            var chips = itemFactory.CreateItem<Chips>(new string[] { "chips", "1.0" });
+
+            order.Items.Add(drink);
+            order.Items.Add(chips);
+
+            var discountItem = discounter.GetDiscountItemConditionally(order);
+
+            Assert.IsTrue(discountItem.Type == typeof(DiscountItem).ToString(), "should return discount item");
+        }
+
+        [TestMethod()]
+        public void GetDiscountItemConditionallyTest_HasDrinkButNotChipsDoesReturnsNull()
+        {
+            var order = new Order();
+            var drink = itemFactory.CreateItem<Drink>(new string[] { "drink", "1.0" });
+
+            order.Items.Add(drink);
+
+            var discountItem = discounter.GetDiscountItemConditionally(order);
+
+            Assert.IsNull(discountItem, "should return null");
+        }
+
+        [TestMethod()]
+        public void GetDiscountItemConditionallyTest_HasChipsButNotDrinkDoesReturnsNull()
+        {
+            var order = new Order();
+            var chips = itemFactory.CreateItem<Chips>(new string[] { "chips", "1.0" });
+
+            order.Items.Add(chips);
+
+            var discountItem = discounter.GetDiscountItemConditionally(order);
+
+            Assert.IsNull(discountItem, "should return null");
+        }
+    }
+}
---
+++ src/SandwichOrderSystem/SandwichOrderSystemSharedTests/Services/OrderManagerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SandwichOrderSystemShared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandwichOrderSystemShared.Services.Tests
+{
+    [TestClass()]
+    public class OrderManagerTests
+    {
+        [TestMethod()]
+        public void OrderManagerTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddItemToOrderTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddOrderToOrdersTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ResetCurrentOrderTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ResetOrdersTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FinishOrdersTest()
+        {
+
+        }
+    }
+}
