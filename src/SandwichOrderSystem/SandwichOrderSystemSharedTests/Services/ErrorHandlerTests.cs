335fbcbf3838dbacaca6613a7d8701265840fceb
David Lin <david.lin@bmwna.com>
2017-09-10 13:09:38 -0600

    add ErrorHandlerTests but nothing to test here

---
+++ src/SandwichOrderSystem/SandwichOrderSystemSharedTests/Services/ErrorHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SandwichOrderSystemShared.Services.Tests
+{
+    [TestClass()]
+    public class ErrorHandlerTests
+    {
+        [TestMethod()]
+        public void HandleErrorTest()
+        {
+            // nothing to test
+            Assert.IsTrue(true);
+        }
+    }
+}
